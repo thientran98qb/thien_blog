766de1d1c2d0cfc5eda39afb7bf6065d47183bb4
tomosia_tran_nam <tran.nam@tomosia.com>
2021-12-22 23:40:13 +0700

    admin template

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# thien_blog
+# Install Core UI : 
+    npm i @coreui
+    npm i @coreui/icons
+# webpack.mix.js: 
+    mix.copy('node_modules/@coreui/coreui/dist/js/coreui.bundle.min.js', 'public/js');
+    mix.copy('node_modules/@coreui/icons/fonts', 'public/fonts');
+    mix.copy('node_modules/@coreui/icons/css/free.min.css', 'public/css');
+    mix.copy('node_modules/@coreui/icons/css/brand.min.css', 'public/css');
+    mix.copy('node_modules/@coreui/icons/css/flag.min.css', 'public/css');
+# app.sccc
+    @import "~@coreui/coreui/scss/coreui";
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,4 @@
 const mix = require('laravel-mix');
-const tailwindcss = require('tailwindcss');
 
 /*
  |--------------------------------------------------------------------------
@@ -7,13 +6,16 @@ const tailwindcss = require('tailwindcss');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
-    .options({
-        postCss: [ tailwindcss('./tailwind.config.js') ],
-    });
+    .vue()
+    .sass('resources/sass/app.scss', 'public/css');
+mix.copy('node_modules/@coreui/coreui/dist/js/coreui.bundle.min.js', 'public/js');
+mix.copy('node_modules/@coreui/icons/fonts', 'public/fonts');
+mix.copy('node_modules/@coreui/icons/css/free.min.css', 'public/css');
+mix.copy('node_modules/@coreui/icons/css/brand.min.css', 'public/css');
+mix.copy('node_modules/@coreui/icons/css/flag.min.css', 'public/css');
